1de9c65d5aa59697239ba9d2569e7c6d0aac200f
Heidi Huang <heidihuang@google.com>
2026-07-13 10:34:14 +0000

    compute: make nat_subnets and consumer_reject_lists sets on google_compute_service_attachment

--- mmv1/third_party/terraform/services/compute/resource_compute_service_attachment_test.go
+++ mmv1/third_party/terraform/services/compute/resource_compute_service_attachment_test.go
@@ -81,6 +81,10 @@ func TestAccComputeServiceAttachment_serviceAttachmentBasicExampleUpdate(t *test
 				// send_propagated_connection_limit_if_zero is a virtual field with no API
 				// representation, so it can't be reconstructed on import.
 				ImportStateVerifyIgnore: []string{"target_service", "region", "send_propagated_connection_limit_if_zero"},
+      },
+			{
+				Config:   testAccComputeServiceAttachment_serviceAttachmentBasicExampleUpdateReordered(context),
+				PlanOnly: true,
 			},
 		},
 	})
@@ -977,6 +981,54 @@ resource "google_compute_service_attachment" "psc_ilb_service_attachment" {
 resource "google_compute_forwarding_rule" "psc_ilb_target_service" {
   name                  = "tf-test-fr-%{random_suffix}"
   region                = "us-west2"
+func testAccComputeServiceAttachment_serviceAttachmentBasicExampleUpdateReordered(context map[string]interface{}) string {
+	// Swaps the order of consumer_reject_lists to verify that Sets do not track order
+	return acctest.Nprintf(`
+resource "google_compute_service_attachment" "psc_ilb_service_attachment" {
+  name        = "tf-test-my-psc-ilb%{random_suffix}"
+  region      = "us-west2"
+  description = "A service attachment configured with Terraforms"
+
+  enable_proxy_protocol    = false
+  connection_preference    = "ACCEPT_MANUAL"
+  nat_subnets              = [google_compute_subnetwork.psc_ilb_nat.id]
+  target_service           = google_compute_forwarding_rule.psc_ilb_target_service.id
+
+  # Order is deliberately swapped compared to the previous step
+  consumer_reject_lists = ["482878270665", "673497134629"]
+  
+  consumer_accept_lists {
+    project_id_or_num = "658859330310"
+    connection_limit  = 4
+  }
+  reconcile_connections = false
+
+  propagated_connection_limit = 0
+  send_propagated_connection_limit_if_zero = true
+}
+
+resource "google_compute_address" "psc_ilb_consumer_address" {
+  name   = "tf-test-psc-ilb-consumer-address%{random_suffix}"
+  region = "us-west2"
+
+  subnetwork   = "default"
+  address_type = "INTERNAL"
+}
+
+resource "google_compute_forwarding_rule" "psc_ilb_consumer" {
+  name   = "tf-test-psc-ilb-consumer-forwarding-rule%{random_suffix}"
+  region = "us-west2"
+
+  target                = google_compute_service_attachment.psc_ilb_service_attachment.id
+  load_balancing_scheme = "" # need to override EXTERNAL default when target is a service attachment
+  network               = "default"
+  ip_address            = google_compute_address.psc_ilb_consumer_address.id
+}
+
+resource "google_compute_forwarding_rule" "psc_ilb_target_service" {
+  name   = "tf-test-producer-forwarding-rule%{random_suffix}"
+  region = "us-west2"
+
   load_balancing_scheme = "INTERNAL"
   backend_service       = google_compute_region_backend_service.producer_service_backend.id
   all_ports             = true
@@ -993,6 +1045,15 @@ resource "google_compute_region_backend_service" "producer_service_backend" {
 
 resource "google_compute_health_check" "producer_service_health_check" {
   name               = "tf-test-hc-%{random_suffix}"
+  name   = "tf-test-producer-service%{random_suffix}"
+  region = "us-west2"
+
+  health_checks = [google_compute_health_check.producer_service_health_check.id]
+}
+
+resource "google_compute_health_check" "producer_service_health_check" {
+  name = "tf-test-producer-service-health-check%{random_suffix}"
+
   check_interval_sec = 1
   timeout_sec        = 1
   tcp_health_check {
@@ -1002,12 +1063,16 @@ resource "google_compute_health_check" "producer_service_health_check" {
 
 resource "google_compute_network" "psc_ilb_network" {
   name                    = "tf-test-net-%{random_suffix}"
+  name = "tf-test-psc-ilb-network%{random_suffix}"
   auto_create_subnetworks = false
 }
 
 resource "google_compute_subnetwork" "psc_ilb_producer_subnetwork" {
   name          = "tf-test-prod-sub-%{random_suffix}"
   region        = "us-west2"
+  name   = "tf-test-psc-ilb-producer-subnetwork%{random_suffix}"
+  region = "us-west2"
+
   network       = google_compute_network.psc_ilb_network.id
   ip_cidr_range = "10.0.0.0/16"
 }
@@ -1017,6 +1082,8 @@ resource "google_compute_subnetwork" "psc_ilb_nat" {
   region        = "us-west2"
   network       = google_compute_network.psc_ilb_network.id
   purpose       = "PRIVATE_SERVICE_CONNECT"
+  name   = "tf-test-psc-ilb-nat%{random_suffix}"
+  region = "us-west2"
   ip_cidr_range = "10.1.0.0/16"
 }
 `, context)
